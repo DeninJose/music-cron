9d3bae72fef89e348eea095643fa8409f6279124
Denin Jose <deninjose8@gmail.com>
2025-12-21 11:00:13 +0530

    Added log redirection

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 ## REPLACE WITH PROJECT DIR
 SCRIPT_PATH="$HOME/Documents/code/bash/music-cron"
-CRON_SCHEDULE="0 5 * * 0"
+CRON_SCHEDULE="0 12 * * 0"
 
 # Command to add to cron table
-CRON_JOB_ENTRY="$CRON_SCHEDULE /bin/bash $SCRIPT_PATH/download.sh"
+CRON_JOB_ENTRY="$CRON_SCHEDULE /bin/bash $SCRIPT_PATH/download.sh >> $SCRIPT_PATH/cron.log 2>&1"
 
 # Add the cron job
 (crontab -l 2>/dev/null; echo "$CRON_JOB_ENTRY") | crontab -
