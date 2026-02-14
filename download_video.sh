4c5c7029e8eafb6c0fe7383291eff9bb01a5e7a9
Denin Jose <deninjose8@gmail.com>
2026-02-14 13:11:15 +0530

    Simpler video download to ensure consistent downloads

--- download_video.sh
+++ download_video.sh
@@ -6,7 +6,5 @@ DOWNLOAD_PATH="$HOME/Documents/Videos/yt"
 
 yt-dlp \
     --download-archive "$DOWNLOAD_PATH/downloaded_videos.txt" \
-    -f "bv*+ba/b" \
-    --merge-output-format mp4 \
     -o "$DOWNLOAD_PATH/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" \
     -a "$SCRIPT_PATH/video_playlists.txt"
